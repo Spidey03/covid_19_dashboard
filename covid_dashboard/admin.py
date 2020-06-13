49f990e4b3bd09f8af0555bcdb5119b7af0619ab
Naveen <sayhai2naveenkumar@gmail.com>
2020-06-13 04:05:35 +0000

    Setup completed for version 2

--- covid_dashboard/admin.py
+++ covid_dashboard/admin.py
@@ -1,12 +1,1 @@
 # your django admin
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from covid_dashboard.models\
-    import User, Stats, Mandal, District, State
-# Register your models here.
-
-admin.site.register(User, UserAdmin)
-admin.site.register(Stats)
-admin.site.register(Mandal)
-admin.site.register(District)
-admin.site.register(State)
--- covid_dashboard/models/__init__.py
+++ covid_dashboard/models/__init__.py
@@ -1,10 +1,4 @@
-from covid_dashboard.models.user import User
-from covid_dashboard.models.places\
-    import Mandal, District, State
-from covid_dashboard.models.covid_case import Stats
-
-
-__all__ = ["User","Mandal","District","State", "Stats"]
+__all__ = []
 
 # class DummyModel(AbstractDateTimeModel):
 #     """
